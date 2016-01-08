004af2b5666a30d9dffbf685f5a87845ada76a4e
Arun <arun.rs@identitymine.com>
2016-01-08 18:56:49 +0530

    adding interaction

---
+++ app.js
@@ -0,0 +1,271 @@
+var json = {
+    "Districts": {
+        "District": [
+            {
+                "Name": "Kasargod",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kasrg",
+                        "Type": "Type1_kasrg"
+                    },
+                    {
+                        "SpotName": "Spot2_kasrg",
+                        "Type": "Type2_kasrg"
+                    },
+                    {
+                        "SpotName": "Spot3_kasrg",
+                        "Type": "Type3_kasrg"
+                    }
+                ]
+            },
+            {
+                "Name": "Kannur",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kannur",
+                        "Type": "Type1_kannur"
+                    },
+                    {
+                        "SpotName": "Spot2_kannur",
+                        "Type": "Type2_kannur"
+                    },
+                    {
+                        "SpotName": "Spot3_kannur",
+                        "Type": "Type3_kannur"
+                    }
+                ]
+            },
+            {
+                "Name": "Kozhikode",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Kottayam",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Ernakulam",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Thrissur",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Palakkad",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Wayanad",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Malappuram",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Kollam",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Trivandrum",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Pathanamthitta",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Alappuzha",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_kozhikode",
+                        "Type": "Type2_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot3_kozhikode",
+                        "Type": "Type3_kozhikode"
+                    }
+                ]
+            },
+            {
+                "Name": "Idukki",
+                "Spot": [
+                    {
+                        "SpotName": "Spot1_kozhikode",
+                        "Type": "Type1_kozhikode"
+                    },
+                    {
+                        "SpotName": "Spot2_idukki",
+                        "Type": "Type2_idukki"
+                    },
+                    {
+                        "SpotName": "Spot3_idukki",
+                        "Type": "Type3_idukki"
+                    }
+                ]
+            }
+        ]
+    }
+};
+var spots;
+var types;
+window.onload = function () {
+    var districts = document.querySelectorAll(".grow");
+    for (var i = 0; i < districts.length; i++) {
+        var district = districts.item(i);
+        alertMessage(district);
+    }
+    spots = document.getElementById('spots');
+};
+function alertMessage(x) {
+    x && x.addEventListener("click", function () {
+        spots.innerHTML = "";
+        spots.innerHTML += "<h2>Spots</h2>";
+        for (var _i = 0, _a = json.Districts.District; _i < _a.length; _i++) {
+            var district = _a[_i];
+            if (district.Name === x.id.toString()) {
+                var spotArray = district.Spot;
+                for (var _b = 0; _b < spotArray.length; _b++) {
+                    var s = spotArray[_b];
+                    spots.innerHTML += "<h4>" + s.SpotName + "</h4>";
+                }
+            }
+        }
+    });
+}
+//# sourceMappingURL=app.js.map
